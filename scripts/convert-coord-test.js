797b7372a56bfe63a1d1db0b60f09fa2e7a665a7
cheeaun <cheeaun@gmail.com>
2021-08-28 08:23:21 +0000

    Deploying to gh-pages from @ cheeaun/railrouter-sg@8f129539cd20cdb3ee5578a228cd412f8cb96c2c 🚀

--- scripts/convert-coord-test.js
+++
@@ -1,3 +0,0 @@
-var conv = require('./svy21.js');
-var res = conv(31993.7065, 29784.3322);
-console.log(res);
--- scripts/from-smrt-recompile-all.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-var allFile = 'data/all.json';
-var allData = JSON.parse(fs.readFileSync(allFile));
-var stopPromises = Object.keys(allData.stops).map(function(key){
-  return new Promise(function(resolve, reject){
-    var stop = allData.stops[key];
-  });
-});
